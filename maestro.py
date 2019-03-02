b08a0309c70ed6c7df2f51d7f326884d3952e9b1
Anthony H <the7bunsoflife@gmail.com>
2019-03-02 13:09:47 -0500

    Added mapData method to Sensor class

--- maestro.py
+++ maestro.py
@@ -4,6 +4,7 @@ import sys
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
+import math
 
 style.use('fivethirtyeight')
 
@@ -72,14 +73,42 @@ class Sensor:
 
     # Store information
     def __init__(self):
-        self.values = list()
-        self.time = list()
+        self.values = list() # Voltage values on the y-axis
+        self.mapped_data = list() # The mapped values from the delta voltage values
+        self.time = list()  # Time values on the x-axis
         self.fig = plt.figure()
         self.ax1 = self.fig.add_subplot(1,1,1)
 
+    def mapData(self, data):
+        delta_y = data - self.values[len(self.values) - 1]
+        x = 0
+        scaling = 2
+
+        if delta_y > 0:
+            x = math.floor(math.log(delta_y/scaling, 2))
+        elif delta_y < 0:
+            x = math.ceil(math.log(delta_y/scaling, 2))
+
+        if x > 4:
+            x = 4
+        elif x < -4:
+            x = -4
+        return x
+
     def addData(self, time_stamp, data):
-        self.values.append(data)
-        self.time.append(time_stamp)
+
+        # Initial instance of the list
+        if len(self.values) == 0:
+            self.values.append(data)
+            self.time.append(time_stamp)
+        else:
+            self.values.append(data)
+            self.time.append(time_stamp)
+            self.mapped_data.append(self.mapData(data))
+
+
+
+
 
     def animate(self,i):
         xs = self.time
