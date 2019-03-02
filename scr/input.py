0a058c664862c9ca7c25264a0ba9bec492f9b778
Dev-Khan <cww904@gmail.com>
2019-03-02 09:53:13 +0000

    Created a scr dir for clean work space

---
+++ scr/input.py
@@ -0,0 +1,62 @@
+import glob
+import serial
+import sys
+
+def panic():
+	print("Oh shit, everything is broken in Serial Land")
+
+
+class Communication_Device:
+	
+	def __init__(self, InType=None, Port=0, BitRate=0, NumSensors=0):
+		
+		# What type of input are we dealing with (Serial, Socket, etc)
+		if InType is None:
+			self.InType, self.Port = ('COM', self.listSerialPorts()[0]) # assume the first port is Arduino 
+		else:
+			self.InType = InType
+			self.Port = Port
+			
+		# Create the Arduino member
+		try:
+			self.arduino = serial.Serial(self.Port)
+		except (OSError, serial.SerialException):
+			panic()
+			
+		
+	
+	def listSerialPorts(self):
+		# http://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+		if sys.platform.startswith('win'):
+			ports = ['COM' + str(i + 1) for i in range(256)]
+		
+		elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+			# this is to exclude your current terminal "/dev/tty"
+			ports = glob.glob('/dev/tty[A-Za-z]*')
+			print(ports)
+		
+		elif sys.platform.startswith('darwin'):
+			ports = glob.glob('/dev/tty.*')
+		
+		else:
+			raise EnvironmentError('Unsupported platform')
+		
+		result = []
+		for port in ports:
+			try:
+				s = serial.Serial(port)
+				print(s)
+				s.close()
+				result.append(port)
+			except (OSError, serial.SerialException):
+				pass
+		return result
+		
+	def read_data_stream(self):
+		while True:
+			data = self.arduino.readline()[:-2].decode("utf-8")
+			if data:
+				print(data)
+
+c = Communication_Device()
+c.read_data_stream()
