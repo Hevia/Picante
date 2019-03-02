815c786a3f091d57ccc092a6ddd8e1126eb59d0f
Anthony H <the7bunsoflife@gmail.com>
2019-03-02 11:26:01 -0500

    Add files via upload

---
+++ maestro.py
@@ -0,0 +1,106 @@
+import glob
+import serial
+import sys
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
+
+style.use('fivethirtyeight')
+
+
+def panic():
+    print("Oh shit, everything is broken in Serial Land")
+
+
+
+
+class Communication_Device:
+
+    def __init__(self, InType=None, Port=0, BitRate=0, NumSensors=0):
+
+        # What type of input are we dealing with (Serial, Socket, etc)
+        if InType is None:
+            self.InType, self.Port = ('COM', self.listSerialPorts()[0])  # assume the first port is Arduino
+        else:
+            self.InType = InType
+            self.Port = Port
+
+        # Create the Arduino member
+        try:
+            self.arduino = serial.Serial(self.Port)  # , baudrate=115200, timeout=0.1) # Port, Baud-Rate, Timeout
+        except (OSError, serial.SerialException):
+            panic()
+
+    def listSerialPorts(self):
+        # http://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+        if sys.platform.startswith('win'):
+            ports = ['COM' + str(i + 1) for i in range(256)]
+
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this is to exclude your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+            print(ports)
+
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                print("Could not open {0}".format(port))
+        return result
+
+    def read_data_stream(self):
+        while True:
+            data = self.arduino.readline()[:-2].decode("utf-8")
+            if data:
+                print(data)
+
+
+
+
+
+class Sensor:
+    # Get figure objects from sensor to the
+
+    # Store information
+    def __init__(self):
+        self.values = list()
+        self.time = list()
+        self.fig = plt.figure()
+        self.ax1 = self.fig.add_subplot(1,1,1)
+
+    def addData(self, time_stamp, data):
+        self.values.append(data)
+        self.time.append(time_stamp)
+
+    def animate(self,i):
+        xs = self.time
+        ys = self.values
+        self.ax1.clear()
+        self.ax1.plot(xs, ys)
+
+    def makeFigure(self):
+        ani = animation.FuncAnimation(self.fig, self.animate, interval=1000)
+        plt.show()
+
+
+s = Sensor()
+s.addData(3295, 512)
+s.addData(3347, 511)
+s.addData(3553, 512)
+s.makeFigure();
+
+
+
+
+#c = Communication_Device()
+#c.read_data_stream()
+
