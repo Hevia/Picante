12061826ea875f01264e92132fc1a579cdaee7d1
Dev-Khan <cww904@gmail.com>
2019-03-02 11:46:28 +0000

    Leap Lib Deps for Python 3

--- scr/leap_input.py
+++ scr/leap_input.py
@@ -1,17 +1,29 @@
-import os, sys, inspect
-src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+import sys
+from leap_python3 import Leap 
 
-# Windows and Linux
-'''
-if sys.maxsize > 2**32:
-	arch_dir = '../LeapSDK/lib/x64'
-else:
-	arch_dir = '../LeapSDK/lib/x86'
 
-lib_dir = os.path.abspath(os.path.join(src_dir, arch_dir))
-sys.path.insert(0, lib_dir)
-'''
-lib_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
-sys.path.insert(0, lib_dir)
-
-import Leap
+class SampleListener(Leap.Listener):
+	
+	def on_connect(self, controller):
+		print("Connected")
+	
+	def on_frame(self, controller):
+		print("Frames avaible")
+		
+		
+def main():
+	listener = SampleListener()
+	controller = Leap.Controller()
+	
+	controller.add_listener(listener)
+	
+	# Keep this process running until Enter is Pressed
+	print("Press Enter to Quit")
+	try:
+		sys.stdin.readline()
+	except KeyboardInterrupt:
+		pass
+	finally:
+		controller.remove_listener(listener)
+		
+main()
