b4bbcf646580b20b407c24bdbbafccbb121bb190
Dev-Khan <cww904@gmail.com>
2019-03-03 07:21:42 +0000

    threading for contiunious data

--- scr/maestro.py
+++ scr/maestro.py
@@ -1,6 +1,15 @@
 from Sensor import *
 from input import *
+import threading
 
 
-s = Sensor()
-c = Communication_Device()
+def main():
+	s = Sensor()
+	c = Communication_Device()
+	
+	inThread = threading.Thread(target=c.read_data_stream, args=())
+	outThread = threading.Thread(target=s.output_data, args=("output.txt", "fancy-out.txt"))# s.output_data("output.txt", "fancy-out.txt")
+	inThread.start()
+	outThread.start()
+	
+main()
