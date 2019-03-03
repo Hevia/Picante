3a63a7d1cff9b08b8f7b7e4befa6ddc2f20d62f9
Anthony H <the7bunsoflife@gmail.com>
2019-03-03 00:15:17 -0500

    Add files via upload

--- scr/Sensor.py
+++ scr/Sensor.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 import math
+import csv
 
 style.use('fivethirtyeight')
 
@@ -10,45 +11,125 @@ class Sensor:
     # Get figure objects from sensor to the
 
     # Store information
-    def __init__(self):
+    def __init__(self, n_sec=0.25, scaling=0.09):
+        self.n_sec = n_sec
+        self.interval = 0.0
+        self.scaling = scaling
         self.values = list() # Voltage values on the y-axis
         self.mapped_data = list() # The mapped values from the delta voltage values
+        self.mapped_time = list() # The ms values converted to seconds
         self.time = list()  # Time values on the x-axis
         self.fig = plt.figure()
         self.ax1 = self.fig.add_subplot(1,1,1)
 
-    def mapData(self, data):
-        delta_y = data - self.values[len(self.values) - 1]
-        x = 0
-        scaling = 2
+    def mapData(self):
+        slope = 0
+        mapped_val = 0
+        loop_interval = self.calc_interval() 
+        loop_interval = math.ceil(loop_interval)
+        loop_interval = int(loop_interval)
 
-        if delta_y > 0:
-            x = math.floor(math.log(delta_y/scaling, 2))
-        elif delta_y < 0:
-            x = math.ceil(math.log(delta_y/scaling, 2))
+        for i in range(0, len(self.values)-1, loop_interval):
+            if i >= len(self.values) or i+loop_interval >= len(self.values):
+                break
+            else:
+                slope = (self.values[i+loop_interval] - self.values[i]) / loop_interval
+                slope /= self.scaling
 
-        if x > 4:
-            x = 4
-        elif x < -4:
-            x = -4
-        return x
+                if slope < 0:
+                    slope = math.floor(slope)
+                elif slope > 0:
+                    slope = math.ceil(slope)
+
+                if slope > 4:
+                    slope = 4
+                elif slope < -4:
+                    slope = -4
+                self.mapped_data.append(int(slope))
+       
+
+    # This calculates the total length of the recording
+    def mapTime(self):
+        return self.time[len(self.time)-1] / 1000
+
+    def calc_hand_stuff(self, f):
+        out = []
+        for line in f:
+            hand_data = line.strip('\n').replace('(', '').replace(')','').replace(' ', '').split(',')
+            left_hand = hand_data[2:5]
+            right_hand = hand_data[5:]
+
+            #print(left_hand)
+            #sum_left = sum_right = 0
+            sum_left = float(left_hand[0])+float(left_hand[1])+float(left_hand[2])
+            
+            sum_right = float(right_hand[0])+float(right_hand[1])+float(right_hand[2])
+
+            if sum_left < 0:
+                out.append(-1)
+            elif sum_left > 0:
+                out.append(1)
+            else:
+                out.append(0)
+
+            if sum_right < 0:
+                out.append(-1)
+            elif sum_right > 0:
+                out.append(1)
+            else:
+                out.append(0)
+
+        return out
+
+    def calc_interval(self):
+        print(self.n_sec)
+        print((len(self.values)/self.mapTime()))
+        self.interval = self.n_sec * (len(self.values)/self.mapTime())
+        return self.interval
 
     def addData(self, time_stamp, data):
+        self.values.append(data)
+        self.time.append(time_stamp)
 
-        # Initial instance of the list
-        if len(self.values) == 0:
-            self.values.append(data)
-            self.time.append(time_stamp)
-        else:
-            self.values.append(data)
-            self.time.append(time_stamp)
-            self.mapped_data.append(self.mapData(data))
+        # We won't calculate data until we hit certain intervals of the list
+        # if len(self.values) <= self.interval:
+        #     self.values.append(data)
+        #     self.time.append(time_stamp)
+        # else:
+        #     self.values.append(data)
+        #     self.time.append(time_stamp)
+        #     self.mapped_data.append(self.mapData(data))
+        #     self.mapped_time.append(self.mapTime(time_stamp))
 
+
+    def readData(self, file_name):
+        f = open(file_name, 'r')
+        for line in f:
+            cols = line.split(',')
+            cols[1].strip('\n')
+            self.addData(int(cols[1]), int(cols[0]))
+        self.mapData()
+
+    def process_data(self, file_in):
+        retval = []
+        self.readData(file_in)
+        retval.append(self.mapped_data)
+        retval.append(self.calc_hand_stuff(open(file_in, 'r')))
+
+        return retval
+
+    def output_data(self, fIn, fOut):
+        processed = self.process_data(fIn)
+        print(processed[0])
+        with open(fOut, 'w') as f:
+            for i in range(len(processed[0])):
+                f.write("{0}, {1}, {2}\n".format(processed[0][i],processed[1][i], processed[1][i+1]))
+        f.close()
 
 
     def animate(self,i):
-        xs = self.time
-        ys = self.values
+        xs = self.mapped_time
+        ys = self.mapped_data
         self.ax1.clear()
         self.ax1.plot(xs, ys)
 
@@ -58,8 +139,11 @@ class Sensor:
 
 
 # Test Code
-# s = Sensor()
-# s.addData(3295, 512)
-# s.addData(3347, 511)
-# s.addData(3553, 512)
-# s.makeFigure();
+s = Sensor()
+# s.readData("test1.csv")
+# #s.makeFigure()
+# #print(len(s.values))
+# print(s.mapped_data)
+# print(s.mapTime())
+# print(s.calc_interval())
+s.output_data("output.txt", "fancy-out.txt")
